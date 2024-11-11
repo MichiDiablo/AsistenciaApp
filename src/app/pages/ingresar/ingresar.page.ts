93a55cef3bdec3fb41797c7d61708e8ff91c7cbd
César Palacios <ce.palacios@duocuc.cl>
2024-11-11 18:02:51 -0300

    a
    a

---
+++ src/app/pages/ingresar/ingresar.page.ts
@@ -0,0 +1,33 @@
+import { Component, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { ViewWillEnter } from '@ionic/angular';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { LanguageComponent } from 'src/app/components/language/language.component';
+import { Router } from '@angular/router';
+import { colorWandOutline } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './ingresar.page.html',
+  styleUrls: ['./ingresar.page.scss'],
+  standalone: true,
+  imports: [
+      CommonModule            // CGV-Permite usar directivas comunes de Angular
+    , FormsModule             // CGV-Permite usar formularios
+    , IonicModule             // CGV-Permite usar componentes de Ionic como IonContent, IonItem, etc.
+    , TranslateModule         // CGV-Permite usar pipe 'translate'
+    , LanguageComponent // CGV-Lista de idiomas
+  ]
+})
+export class IngresarPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
